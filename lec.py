753a5e350a5cbf2f5435817d8424994aba659574
Илья Насалевич <99810114+ILYA-NASA@users.noreply.github.com>
2022-05-18 21:48:31 +0300

    hi python

---
+++ lec.py
@@ -0,0 +1,48 @@
+# print('hello world')
+
+
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+# value = None
+# print(type(value))
+# print(type(a))
+# print(type(b))
+# value = 12345
+# print(type(value))
+# s = 'hello world'
+# a = 123
+# b = 1.23
+# # print(s) # вывод строки
+# print(a, '-', b, '-', s)
+# print('{1} - {2} - {0}'.format(a, b, s))
+# print(f'{a} - {b} - {s}')
+
+# f = False
+# print(f)
+
+# list = [1, 2.4, '3', 'hi']
+# print(list)
+
+# print('Введите a')
+# a = int(input())
+# print('Введите b')
+# b = float(input())
+# print(a, '+', b, '=', a+b)
+# print('{} {}'.format(a,b))
+# print(f'{a} {b}')
+
+
+# Арифметические операции
+# +, -, *, /, %, //, **
+# **, ___________________
+# (), Сокращенные операции
+
+# a = 1.3123123
+# b = 3
+# c = round(a * b, 10)
+# print(c)
+
+a = 3
+a *= 5
+print(a)
