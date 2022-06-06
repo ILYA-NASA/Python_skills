ea8be2dee1db04c6b55337591d00561116a5657e
Илья Насалевич <99810114+ILYA-NASA@users.noreply.github.com>
2022-06-06 09:37:04 +0300

    correct solutions of task

--- leson8.py
+++ leson8.py
@@ -5,10 +5,7 @@
 #     str_info = f'{name}, {age} год(а), проживает в городе {city}'
 #     return str_info
 
-# user_name = 'Василий'
-# user_age = 21
-# user_city = 'Москва'
-# print(find_info(user_name, user_age, user_city))
+# print(find_info('Василий', 21, 'Москва'))
 
 
 # 2: Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
@@ -27,14 +24,41 @@
 # ### В теле функция должна получить параметр damage атакующего и отнять это количество от health атакуемого. 
 # Функция должна сама работать со словарями и изменять их значения.
 
-# player = {'name': input('Enter players name: '), 'health': 100, 'damage': 30}
-# enemy = {'name': input('Enter enemys name: '), 'health': 100, 'damage': 20}
+# # мое решение
+# player = {
+#     'name': input('Enter players name: '), 
+#     'health': 100, 
+#     'damage': 30}
+# enemy = {
+#     'name': input('Enter enemys name: '), 
+#     'health': 100, 
+#     'damage': 20}
 
 # def attack(person1, person2):
 #     person2['health'] = person2.get('health') - person1.get('damage')
 #     return person1, person2
 
 # print(attack(player, enemy))
+
+# # решение лектора
+# player = {
+#     'name': input('Enter players name: '), 
+#     'health': 100, 
+#     'damage': 30}
+# enemy = {
+#     'name': input('Enter enemys name: '), 
+#     'health': 100, 
+#     'damage': 20}
+
+# def attack(person1, person2):
+#     person2['health'] -= person1['damage']
+
+# # attack(player, enemy)
+# # print(enemy)
+
+# # attack(enemy, player)
+# # print(player)
+
 
 # 4: Давайте усложним предыдущее задание. 
 # Измените сущности, добавив новый параметр - armor = 1.2 (величина брони персонажа)
@@ -46,15 +70,43 @@
 # Примечание. 
 # Функция номер 2 используется внутри функции номер 1 для вычисления урона и вычитания его из здоровья персонажа. 
 
-player = {'name': input('Enter players name: '), 'health': 100, 'damage': 30, 'armor': 1.2}
-enemy = {'name': input('Enter enemys name: '), 'health': 100, 'damage': 20, 'armor': 1.2}
+# # мое решение (так себе)
+# player = {'name': input('Enter players name: '), 'health': 100, 'damage': 30, 'armor': 1.2}
+# enemy = {'name': input('Enter enemys name: '), 'health': 100, 'damage': 20, 'armor': 1.2}
 
-def split_damage(person1):
-    person1['damage'] = person1.get('damage') / person1.get('armor')
-    return person1
+# def split_damage(person1):
+#     person1['damage'] = person1.get('damage') / person1.get('armor')
+#     return person1
+
+# def attack(person1, person2):
+#     person2['health'] = person2.get('health') - split_damage(person1).get('damage')
+#     return person1, person2
+
+# print(attack(player, enemy))
+
+# решение лектора
+player = {
+    'name': input('Enter players name: '), 
+    'health': 100, 
+    'damage': 30,
+    'armor': 1.2
+    }
+enemy = {
+    'name': input('Enter enemys name: '), 
+    'health': 100, 
+    'damage': 20, 
+    'armor': 1
+    }
+
+def split_damage(damage, armor):
+    return damage / armor
 
 def attack(person1, person2):
-    person2['health'] = person2.get('health') - split_damage(person1).get('damage')
-    return person1, person2
+    damage = split_damage(person1['damage'], person2['armor'])
+    person2['health'] -= damage
 
-print(attack(player, enemy))
+attack(player, enemy)
+print(enemy)
+
+attack(enemy, player)
+print(player)
